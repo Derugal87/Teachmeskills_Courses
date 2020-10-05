3aa2c0a7685488fcff884601d2a4f0ddfdcbe5ed
Derugal87 <60985658+Derugal87@users.noreply.github.com>
2020-10-05 13:16:18 +0300

    Delete task_3_4.py

--- task_3_4.py
+++
@@ -1,7 +0,0 @@
-string = input('Введите строку: ')
-length = len(string)
-length_center = int(length / 2)
-if string[length_center] == string[0]:
-    print(string[1:-1])
-else:
-    print(string[length_center])
