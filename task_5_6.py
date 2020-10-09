2668c6096429f83dfaaea44ef7530c30648cee9a
Alexander Derugo <alexander.derugo@gmail.com>
2020-10-09 21:11:09 +0300

    final I think

--- task_5_6.py
+++ task_5_6.py
@@ -1,22 +1,20 @@
-from typing import List
-
-
-def operation(some_list: List[int]):
+def operation(some_list):
     count = 0
     index = 0
-    for i in range(len(some_list)):
-        if some_list[i] - some_list[index] == 1:
-            if i != len(some_list) - 1:
-                if some_list[i] > some_list[i + 1] or some_list[i + 1] - some_list[i] > 1:
+    for i in range(1, len(some_list)):
+        if i != len(some_list) - 1:
+            if not(some_list[i] - some_list[index] != 1):
+                if some_list[i + 1] - some_list[i] != 1:
                     count += 1
                     index = i
             else:
-                count += 1
+                index = i
         else:
-            index = i
+            if some_list[i] - some_list[i - 1] == 1:
+                count += 1
     return f'\nNumber of monotone areas = {count}'
 
 
 if __name__ == '__main__':
-    print(operation([1,2, 1, 2,4,5,4,5]))
-    # print(operation([int(input('Enter element: \n')) for i in range(int(input('Enter number of elements: \n')))]))
+    # print(operation([1,2,6,1,2,7,8,9]))
+    print(operation([int(input('Enter element: \n')) for i in range(int(input('Enter number of elements: \n')))]))
