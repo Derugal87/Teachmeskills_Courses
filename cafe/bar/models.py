85a0e2dcff852e90d0d6511c17aba57ceb311db1
Alexander Derugo <alexander.derugo@gmail.com>
2020-11-28 15:22:23 +0300

    homework_15

---
+++ cafe/bar/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    salary = models.FloatField()
+
+
+class Reward(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    foundation_date = models.DateTimeField()
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    address = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=9)
+    age = models.IntegerField(not None)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class RewardEmployee(models.Model):
+    reward = models.ForeignKey(Reward, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    reward_date = models.DateTimeField()
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=9)
+
+
+class Menu(models.Model):
+    description = models.CharField(max_length=200)
+    price = models.FloatField(not None)
+
+
+class Order(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    client = models.OneToOneField(Client, on_delete=models.CASCADE)
+    reward_date = models.DateTimeField()
+    salary = models.FloatField(not None)
